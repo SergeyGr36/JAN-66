e97b563d29d6ad85df916a0af5e4f972abecd236
Zhurylo Mykhailo <dir2000@ukr.net>
2019-05-13 19:05:37 +0300

    JAN-7 add fields and accessors to Customer class

--- developers-team/src/main/java/com/ra/janus/developersteam/entity/Customer.java
+++ developers-team/src/main/java/com/ra/janus/developersteam/entity/Customer.java
@@ -1,4 +1,22 @@
 package com.ra.janus.developersteam.entity;
 
 public class Customer {
+    private long id;
+    private final String name;
+
+    public Customer(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
