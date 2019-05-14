0a33d77e80d8599a8f83d71c270afeff26ffd888
Zhurylo Mykhailo <dir2000@ukr.net>
2019-05-14 20:55:29 +0300

    JAN-7 add field address to Customer, add constructor

--- developers-team/src/main/java/com/ra/janus/developersteam/entity/Customer.java
+++ developers-team/src/main/java/com/ra/janus/developersteam/entity/Customer.java
@@ -3,6 +3,20 @@ package com.ra.janus.developersteam.entity;
 public class Customer {
     private long id;
     private String name;
+    private String address;
+
+    public Customer(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 
     public long getId() {
         return id;
