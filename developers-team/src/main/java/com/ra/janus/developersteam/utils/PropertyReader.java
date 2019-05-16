b7a519affd8fd091043330f11c60abd8a44f222b
Andrii Riabchuk <andrew.ryabchuk@gmail.com>
2019-05-16 06:55:56 +0000

    deleted redundant class HTwo*

--- developers-team/src/main/java/com/ra/janus/developersteam/utils/PropertyReader.java
+++ developers-team/src/main/java/com/ra/janus/developersteam/utils/PropertyReader.java
@@ -46,49 +46,4 @@ public enum PropertyReader {
 
         return properties;
     }
-
-    public static final class HTwoConnectionFactory implements ConnectionFactory {
-        private static HTwoConnectionFactory connFactory;
-        private static JdbcDataSource dataSource;
-        private static Properties dbProperties = new Properties();
-
-        private HTwoConnectionFactory() throws IOException {
-            loadProperties();
-        }
-
-        /**
-         * this method create Property and load data from config.properties file.
-         */
-        private void loadProperties() throws IOException {
-            dbProperties.load(Objects.requireNonNull(ClassLoader.getSystemResourceAsStream("config.properties")));
-        }
-
-        /**
-         * Method to produce singleton config factory.
-         *
-         * @return ConnectionFactory instance.
-         */
-        public static HTwoConnectionFactory getInstance() throws IOException {
-            synchronized (HTwoConnectionFactory.class) {
-                if (connFactory == null) {
-                    connFactory = new HTwoConnectionFactory();
-                    dataSource = new JdbcDataSource();
-                    dataSource.setURL(dbProperties.getProperty("db.url"));
-                    dataSource.setUser(dbProperties.getProperty("db.username"));
-                    dataSource.setPassword(dbProperties.getProperty("db.password"));
-                }
-            }
-            return connFactory;
-        }
-
-
-        /**
-         * Returns new config.
-         *
-         * @return Connection.
-         */
-        public Connection getConnection() throws SQLException {
-            return dataSource.getConnection();
-        }
-    }
 }
