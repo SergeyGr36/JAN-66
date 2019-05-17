9dd64936b627810e538eae5f7a024e25b4041734
Oleg Kosenkov <o.kosenkov357@gmail.com>
2019-05-17 07:23:48 +0300

    JAN-32 DataSourceFactory created for h2 in memory mode, and test.

---
+++ train-tickets/src/test/java/com/ra/course/janus/traintickets/dao/classes/DataSourceFactoryTest.java
@@ -0,0 +1,18 @@
+package com.ra.course.janus.traintickets.dao.classes;
+
+import org.junit.jupiter.api.Test;
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+class DataSourceFactoryTest {
+
+    @Test
+    void getDataSource() throws SQLException {
+        assertAll(() -> {
+            DataSource ds = DataSourceFactory.H2_IN_MEMORY.getDataSource();
+            ds.getConnection();
+        });
+    }
+}
