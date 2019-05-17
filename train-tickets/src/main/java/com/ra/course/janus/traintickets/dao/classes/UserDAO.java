f43cdf2a63ab05932e0c15fc28e032ad4c85da24
Oleg Kosenkov <o.kosenkov357@gmail.com>
2019-05-17 07:48:32 +0300

    JAN-18 UserDao updated.

--- train-tickets/src/main/java/com/ra/course/janus/traintickets/dao/classes/UserDAO.java
+++ train-tickets/src/main/java/com/ra/course/janus/traintickets/dao/classes/UserDAO.java
@@ -1,14 +1,39 @@
 package com.ra.course.janus.traintickets.dao.classes;
 
-import com.ra.course.janus.traintickets.dao.interfaces.DAO;
+import com.ra.course.janus.traintickets.dao.api.DAO;
 import com.ra.course.janus.traintickets.entity.User;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class UserDAO implements DAO<User> {
+    private final DataSource dataSource;
+
+    private final String INSERT_USER = "insert into USERS (name, id) values (?, ?)";
+    private final String SELECT_USER = "SELECT * FROM USERS WHERE id = ?";
+
+
+    public UserDAO(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Override
-    public User save(User item) {
-        return null;
+    public User save(User user) {
+        try (Connection connection = dataSource.getConnection()) {
+            connection.setAutoCommit(false);
+            PreparedStatement userStatement = connection.prepareStatement(INSERT_USER);
+            userStatement.setString(1, user.getName());
+            userStatement.setLong(2, user.getId());
+            userStatement.executeUpdate();
+            connection.commit();
+        } catch (SQLException e) {
+            throw new IllegalArgumentException(e);
+        }
+        return user;
     }
 
     @Override
@@ -23,7 +48,20 @@ public class UserDAO implements DAO<User> {
 
     @Override
     public User findById(Long id) {
-        return null;
+        try (Connection conn = dataSource.getConnection()) {
+            PreparedStatement userStatement = conn.prepareStatement(SELECT_USER);
+            userStatement.setLong(1, id);
+            User user = new User();
+            ResultSet rs = userStatement.executeQuery();
+            if (rs.next()) {
+                user.setId(id);
+                user.setName(rs.getString("USERNAME"));
+            }
+
+            return user;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
--- train-tickets/src/test/java/com/ra/course/janus/traintickets/dao/classes/UserDAOTest.java
+++ train-tickets/src/test/java/com/ra/course/janus/traintickets/dao/classes/UserDAOTest.java
@@ -1,13 +1,54 @@
 package com.ra.course.janus.traintickets.dao.classes;
 
+import com.ra.course.janus.traintickets.entity.User;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import javax.sql.DataSource;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 class UserDAOTest {
 
+    private static final Long USER_ID = 15L;
+    private static final String USER_NAME = "test_user";
+    private static final DataSource DATA_SOURCE = DataSourceFactory.H2_IN_MEMORY.getDataSource();
+
+    private User user;
+
+    @BeforeAll
+    public static void setupH2Schema() {
+        try (Connection connection = DATA_SOURCE.getConnection()) {
+            Statement st = connection.createStatement();
+            st.execute("create table users(id long, name varchar(44))");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @BeforeEach
+    public void setup() {
+        user = new User();
+        user.setId(USER_ID);
+        user.setName(USER_NAME);
+    }
+
     @Test
     void save() {
+        UserDAO userDAO = new UserDAO(DATA_SOURCE);
+        userDAO.save(user);
+        ResultSet rs;
+        try (Connection connection = DATA_SOURCE.getConnection()) {
+            Statement st = connection.createStatement();
+            rs = st.executeQuery("SELECT * FROM USERS");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
     @Test
