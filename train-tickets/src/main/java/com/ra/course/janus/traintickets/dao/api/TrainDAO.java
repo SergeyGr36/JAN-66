162a96a20082cccda21d38dc0e007795a5f5c3e8
Misha <mishabeheka@gmail.com>
2019-05-21 13:20:27 +0300

    JAN-19 add metod "save" in class TrainDAO

--- train-tickets/src/main/java/com/ra/course/janus/traintickets/dao/api/TrainDAO.java
+++ train-tickets/src/main/java/com/ra/course/janus/traintickets/dao/api/TrainDAO.java
@@ -2,12 +2,36 @@ package com.ra.course.janus.traintickets.dao.api;
 
 import com.ra.course.janus.traintickets.entity.Train;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 public class TrainDAO implements DAO<Train> {
+
+    private final DataSource dataSource;
+
+    private final String INSERT_TRAIN = "insert into Trains (name, id) values (?, ?)";
+    private final String SELECT_TRAIN = "SELECT * FROM USERS WHERE id = ?";
+
+    public TrainDAO(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Override
-    public Train save(Train item) {
-        return null;
+    public Train save(Train train) {
+        try(Connection connection = dataSource.getConnection()){
+            connection.setAutoCommit(false);
+            PreparedStatement trainStatement = connection.prepareStatement(INSERT_TRAIN);
+            trainStatement.setString(1,train.getName());
+            trainStatement.setLong(2,train.getId());
+            trainStatement.executeUpdate();
+            connection.commit();
+        }catch (SQLException e){
+            throw new IllegalArgumentException();
+        }
+        return train;
     }
 
     @Override
