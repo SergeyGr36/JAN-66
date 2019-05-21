14f9d4c4ad3764820f739297797351ddd44ebcf4
Misha <mishabeheka@gmail.com>
2019-05-21 12:59:58 +0300

    JAN-19 fix class Train

--- train-tickets/src/main/java/com/ra/course/janus/traintickets/entity/Train.java
+++ train-tickets/src/main/java/com/ra/course/janus/traintickets/entity/Train.java
@@ -8,4 +8,35 @@ public class Train  {
 
     private String name;
 
+    private int numberOfPlaces;
+
+    public Train(long id, String name, int numberOfPlaces) {
+        this.id = id;
+        this.name = name;
+        this.numberOfPlaces = numberOfPlaces;
     }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumberOfPlaces() {
+        return numberOfPlaces;
+    }
+
+    public void setNumberOfPlaces(int numberOfPlaces) {
+        this.numberOfPlaces = numberOfPlaces;
+    }
+}
