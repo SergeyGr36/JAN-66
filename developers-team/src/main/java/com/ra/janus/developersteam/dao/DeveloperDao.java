c858451da4d64d4e359bc3d11d5ccd3bd9cda890
Oleksii Riumin <Oleksii.Riumin@synapse.com>
2019-05-22 15:21:56 +0300

    Developer: implemented some methods

--- developers-team/src/main/java/com/ra/janus/developersteam/dao/DeveloperDao.java
+++ developers-team/src/main/java/com/ra/janus/developersteam/dao/DeveloperDao.java
@@ -1,6 +1,90 @@
 package com.ra.janus.developersteam.dao;
 
-public class DeveloperDao {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.sql.DataSource;
 
+import com.ra.janus.developersteam.entity.Developer;
+import com.ra.janus.developersteam.exception.DAOException;
+import com.ra.janus.developersteam.interfaces.IBaseDAO;
+
+public class DeveloperDao implements IBaseDAO<Developer> {
+
+    private static final String SELECT_ALL_SQL = "SELECT * FROM DEVELOPER";
+    private static final String UPDATE_SQL = "UPDATE DEVELOPER SET NAME = ? WHERE ID = ?";
+    private static final String DELETE_SQL = "DELETE FROM DEVELOPER WHERE ID = ?";
+    
+    private final DataSource dataSource;
+
+    public DeveloperDao(DataSource dataSource) {
+
+    	this.dataSource = dataSource;
+    }
+    
+	@Override
+	public List<Developer> getAll() {
+
+		try {
+			   Connection conn = dataSource.getConnection();
+               PreparedStatement ps = conn.prepareStatement(SELECT_ALL_SQL);
+               ResultSet rs = ps.executeQuery();
+
+               final List<Developer> developers = new ArrayList<>();
+               while (rs.next()) {
+            	   //developers.add(toWork(rs));
+               }
+
+               return developers;
+           } catch (SQLException e) {
+
+        	   throw new DAOException(e);
+           }
+	}
+
+	@Override
+	public Developer getById(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean update(Developer entity) {
+
+		try {
+        	  Connection conn = dataSource.getConnection();
+              PreparedStatement ps = conn.prepareStatement(UPDATE_SQL);
+              prepareStatement(ps, entity);
+
+               return ps.executeUpdate() != 0;
+           } catch (SQLException e) {
+
+        	   throw new DAOException(e);
+           }
+        }
+
+	@Override
+	public boolean delete(long id) {
+
+		try {
+                Connection conn = dataSource.getConnection();
+                PreparedStatement ps = conn.prepareStatement(DELETE_SQL);
+                ps.setLong(1, id);
+
+            return ps.executeUpdate() != 0;
+        } catch (SQLException e) {
+
+        	throw new DAOException(e);
+        }
+    }
+
+	@Override
+	public boolean create(Developer entity) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 }
--- developers-team/src/main/java/com/ra/janus/developersteam/entity/Developer.java
+++ developers-team/src/main/java/com/ra/janus/developersteam/entity/Developer.java
@@ -2,6 +2,28 @@ package com.ra.janus.developersteam.entity;
 
 public class Developer {
 
-   int id;
+   long id;
    String developerName;
+   
+   public Developer(final long id, final String developerName) {
+	   
+	   this.id = id;
+	   this.developerName = developerName;
+   }
+
+public long getId() {
+	return id;
+}
+
+public void setId(long id) {
+	this.id = id;
+}
+
+public String getDeveloperName() {
+	return developerName;
+}
+
+public void setDeveloperName(String developerName) {
+	this.developerName = developerName;
+}
 }
