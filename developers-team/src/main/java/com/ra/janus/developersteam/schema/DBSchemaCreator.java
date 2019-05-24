143c1d701d4dbf2c651630390d6f49a1a0963df0
Zhurylo Mykhailo <dir2000@ukr.net>
2019-05-24 13:35:27 +0300

    JAN-38 make DBSchemaCreator class

--- developers-team/src/main/java/com/ra/janus/developersteam/schema/DBSchemaCreator.java
+++ developers-team/src/main/java/com/ra/janus/developersteam/schema/DBSchemaCreator.java
@@ -4,20 +4,27 @@ import com.ra.janus.developersteam.utils.PropertyReader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Properties;
-import java.util.stream.Stream;
 
 public class DBSchemaCreator {
-    public static final String EXCEPTION_WARN = "An exception occurred!";
+    private static final String EXCEPTION_WARN = "An exception occurred!";
     private static final String DEFAULT_DIRS = "sql_schema_scripts";
     private static final Logger LOGGER = LoggerFactory.getLogger(DBSchemaCreator.class);
-    private String scriptsDirectories;
+
+    transient private String scriptsDirs;
+    transient private Connection connection;
 
     public DBSchemaCreator() {
         Properties properties;
@@ -25,45 +32,76 @@ public class DBSchemaCreator {
             properties = PropertyReader.INSTANCE.getProperties();
         } catch (IOException e) {
             LOGGER.error(EXCEPTION_WARN, e);
-            throw new IllegalStateException("Could not read the application properties file.");
+            throw new IllegalStateException("Could not read the application properties file.", e);
         }
 
-        scriptsDirectories = properties.getProperty("db.sql_schema_scripts_directories");
-        if (scriptsDirectories == null) {
-            scriptsDirectories = DEFAULT_DIRS;
-        }
-    }
-
-    public void createSchema(Connection connection) {
-        String[] dirNames = scriptsDirectories.split(";");
-        for (String dirName : dirNames) {
-            Path path;
-            try {
-                path = Paths.get(ClassLoader.getSystemResource(dirName).toURI());
-            } catch (URISyntaxException e) {
-                LOGGER.error(EXCEPTION_WARN, e);
-                throw new IllegalStateException("A syntax URI rule violation occurred");
-            }
-
-            if (!Files.exists(path)) {
-                IllegalStateException e = new IllegalStateException("The path doesn't exists " + path);
-                LOGGER.error(EXCEPTION_WARN, e);
-                throw e;
-            }
-
-            if (!Files.isDirectory(path)) {
-                IllegalStateException e = new IllegalStateException("The path is not a directory " + path);
-                LOGGER.error(EXCEPTION_WARN, e);
-                throw e;
-            }
-
-            processDirectory(path);
+        scriptsDirs = properties.getProperty("db.sql_schema_scripts_directories");
+        if (scriptsDirs == null) {
+            scriptsDirs = DEFAULT_DIRS;
         }
     }
 
-    private void processDirectory(Path directoryPath) {
-        //try (Stream<Path> entries = Files.find(directoryPath)){
+    public void createSchema(final Connection connection) {
+        this.connection = connection;
+        final String[] dirNames = scriptsDirs.split(";");
+        Arrays.stream(dirNames).map(this::getDirectoryPath).forEach(this::processDirectory);
+    }
 
-        //}
+    private Path getDirectoryPath(final String dirName) {
+        try {
+            final Path path = Paths.get(ClassLoader.getSystemResource(dirName).toURI());
+            validatePath(path);
+            return path;
+        } catch (URISyntaxException e) {
+            LOGGER.error(EXCEPTION_WARN, e);
+            throw new IllegalStateException("A syntax URI rule violation occurred", e);
+        }
+    }
+
+    private void validatePath(final Path path) {
+        if (!Files.exists(path)) {
+            final IllegalStateException e = new IllegalStateException("The path doesn't exists " + path);
+            LOGGER.error(EXCEPTION_WARN, e);
+            throw e;
+        }
+
+        if (!Files.isDirectory(path)) {
+            final IllegalStateException e = new IllegalStateException("The path is not a directory " + path);
+            LOGGER.error(EXCEPTION_WARN, e);
+            throw e;
+        }
+    }
+
+    private void processDirectory(final Path path) {
+        try {
+            Files.find(path, 100,
+                    (filePath, fileAttributes) -> {
+                        final File file = filePath.toFile();
+                        return !file.isDirectory() &&
+                                file.getName().endsWith(".sql");
+                    }).forEach(this::processFile);
+        } catch (IOException e) {
+            LOGGER.error(EXCEPTION_WARN, e);
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    private void processFile(final Path path) {
+        try {
+            final List<String> lines = Files.readAllLines(path);
+            lines.forEach(this::processLine);
+        } catch (IOException e) {
+            LOGGER.error(EXCEPTION_WARN, e);
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    private void processLine(final String line) {
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate(line);
+        } catch (SQLException e) {
+            LOGGER.error(EXCEPTION_WARN, e);
+            throw new IllegalStateException(e);
+        }
     }
 }
