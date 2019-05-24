35ad4f5f50290188b94407c02eef410246734d85
Zhurylo Mykhailo <dir2000@ukr.net>
2019-05-24 15:00:36 +0300

    JAN-38 create DBSchemaCreatorTest class

---
+++ developers-team/src/test/java/com/ra/janus/developersteam/schema/DBSchemaCreatorTest.java
@@ -0,0 +1,12 @@
+package com.ra.janus.developersteam.schema;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DBSchemaCreatorTest {
+
+    @Test
+    void createSchema() {
+    }
+}
