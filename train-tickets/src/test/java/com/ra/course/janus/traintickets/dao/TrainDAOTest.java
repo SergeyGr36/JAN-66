81ba37e2733070110c2365b59e2df6ec3ea330d5
Misha <mishabeheka@gmail.com>
2019-05-27 14:59:55 +0300

    JAN-19 create new classTest for TrainDAO

---
+++ train-tickets/src/test/java/com/ra/course/janus/traintickets/dao/TrainDAOTest.java
@@ -0,0 +1,28 @@
+package com.ra.course.janus.traintickets.dao;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TrainDAOTest {
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findAll() {
+    }
+}
