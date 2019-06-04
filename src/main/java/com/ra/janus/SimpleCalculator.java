53e0a4cd2c322649aa78df685c14ec3ce6790514
Sergey Chernyk <serg199211@gmail.com>
2019-06-04 23:58:23 +0300

    JAN-21 corrected my errors, as delete some files

---
+++ src/main/java/com/ra/janus/SimpleCalculator.java
@@ -0,0 +1,14 @@
+package com.ra.janus;
+
+public class SimpleCalculator {
+
+    public double sum(final int num, final int num1) {
+        return num + num1;
+    }
+
+    public double sub(final int num, final int num1) {
+        return num - num1;
+    }
+
+
+}
