942f273e2e47b78f88cb9cbd2eeba377cca91b2b
Maria Fedoseeva <maria2000fed@gmail.com>
2019-06-07 20:58:51 +0300

    Classes are updated

---
+++ faculty/src/main/java/com/ra/course/janus/faculty/exception/DaoException.java
@@ -0,0 +1,10 @@
+package com.ra.course.janus.faculty.exception;
+
+public class DaoException extends RuntimeException  {
+    public static final long serialVersionUID = -4841130589162737690L;
+
+
+    public DaoException(final String errorMessage, final Throwable err){
+        super(errorMessage,err);
+    }
+}
