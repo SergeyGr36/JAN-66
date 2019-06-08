2c77fac22c53e9b53e38e9e9a29f4d485209ab8a
Maria Fedoseeva <maria2000fed@gmail.com>
2019-06-08 17:32:46 +0300

    added updated classes

---
+++ faculty/src/test/java/com/ra/course/janus/faculty/dao/DataSourceUtilTest.java
@@ -0,0 +1,16 @@
+package com.ra.course.janus.faculty.dao;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.junit.jupiter.api.Test;
+
+import javax.sql.DataSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DataSourceUtilTest {
+    @Test
+    public void whenGetDataSource()  {
+        DataSource dataSource = DataSourceUtils.getDataSource();
+        assertEquals(true, dataSource instanceof HikariDataSource);
+    }
+}
