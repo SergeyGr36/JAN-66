8a8f9a7a0e26f32341bce9c6c6d69dc8a74814d0
Andrii <andrew.ryabchuk@gmail.com>
2019-06-10 23:38:00 +0300

    JAN 43 added mock tests for data source factory

--- developers-team/src/test/java/com/ra/janus/developersteam/datasources/DataSourceFactoryMockTest.java
+++ developers-team/src/test/java/com/ra/janus/developersteam/datasources/DataSourceFactoryMockTest.java
@@ -2,8 +2,11 @@ package com.ra.janus.developersteam.datasources;
 
 import com.ra.janus.developersteam.configuration.DBProperties;
 import com.ra.janus.developersteam.utils.PropertyReaderUtils;
+import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
 import javax.sql.DataSource;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,19 +17,30 @@ import static org.mockito.Mockito.when;
 public class DataSourceFactoryMockTest {
 
 
-//    @Test
-//    public void whenAskHikariDataSourceFactoryReturnsThat() {
-//
-//        //given
-//        DBProperties dbProperties = new DBProperties();
-//
-//        DataSourceFactory dataSourceFactory = new DataSourceFactory(dbProperties);
-//
-//        //when
-//        when(dataSourceFactory.get()).thenReturn(new HikariDataSource());
-//        DataSource dataSource = dataSourceFactory.get();
-//
-//        //then
-//        assertEquals(true, dataSource instanceof DataSource);
-//    }
+    @Test
+    public void whenCreateDataSourceFactoryThenReturnsDataSourceFactoryInstance() throws Exception {
+
+        //given
+        DBProperties dbProperties = mock(DBProperties.class);
+
+        //when
+        DataSourceFactory dataSourceFactory = new DataSourceFactory(dbProperties);
+
+        //then
+        assertNotNull(dataSourceFactory);
+    }
+
+    @Test
+    public void whenDBPropertiesAreNullThenReturnsHikariDataSource() throws Exception {
+
+        //given
+        DBProperties mockDbProperties = mock(DBProperties.class);
+        DataSourceFactory dataSourceFactory = new DataSourceFactory(mockDbProperties);
+
+        //when
+        final Executable executable = () -> dataSourceFactory.get();
+
+        //then
+        assertThrows(IllegalArgumentException.class, executable);
+    }
 }
