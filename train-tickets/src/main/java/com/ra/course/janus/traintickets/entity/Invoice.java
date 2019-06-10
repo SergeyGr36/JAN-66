6e62d824ae411760df3e664e2d13765ca2d98d4b
Sergey Chernyk <serg199211@gmail.com>
2019-06-10 23:53:15 +0300

    JAN-54 using lombok in Invoice POJO

--- train-tickets/src/main/java/com/ra/course/janus/traintickets/entity/Invoice.java
+++ train-tickets/src/main/java/com/ra/course/janus/traintickets/entity/Invoice.java
@@ -1,46 +1,16 @@
 package com.ra.course.janus.traintickets.entity;
 
-import java.math.BigDecimal;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Invoice {
 	private long id;
 	private BigDecimal price;
 	private String attributes;
 
-
-	public Invoice(long id, BigDecimal price, String attributes) {
-		super();
-		this.id = id;
-		this.price = price;
-		this.attributes = attributes;
 	}
-
-	public Invoice() {
-		super();
-	}
-
-	public long getId() {
-		return id;
-	}
-
-	public void setId(long id) {
-		this.id = id;
-	}
-
-	public BigDecimal getPrice() {
-		return price;
-	}
-
-	public void setPrice(BigDecimal p) {
-		this.price = p;
-	}
-
-	public String getAttributes() {
-		return attributes;
-	}
-
-	public void setAttributes(String a) {
-		attributes = a;
-	}
-
-}
