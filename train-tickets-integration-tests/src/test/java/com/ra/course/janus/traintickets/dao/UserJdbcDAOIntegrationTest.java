ddefd51a9d4a3f5f7d35c23e9da2952b01326829
Oleg Kosenkov <o.kosenkov357@gmail.com>
2019-06-11 18:43:23 +0300

    JAN-51 UserDao integr tests refactored to use Spring JDBC. All passed.

--- train-tickets-integration-tests/src/test/java/com/ra/course/janus/traintickets/dao/UserJdbcDAOIntegrationTest.java
+++ train-tickets-integration-tests/src/test/java/com/ra/course/janus/traintickets/dao/UserJdbcDAOIntegrationTest.java
@@ -6,10 +6,7 @@ import com.ra.course.janus.traintickets.entity.User;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import javax.sql.DataSource;
-import java.sql.Connection;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.Collections;
 import java.util.List;
 
@@ -17,26 +14,25 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = MainSpringConfig.class)
 public class UserJdbcDAOIntegrationTest {
-
-
-
 
     private static final String SQL_SCRIPT_FILE_NAME = "src/test/resources/sql_scripts/create_users_table.sql";
 
     private static final User TEST_USER = new User(null, "testname", "mail", "passwd");
 
     @Autowired
-    private DataSource dataSource;
+    private JdbcTemplate jdbcTemplate;
 
     @Autowired
     private UserJdbcDAO userDAO;
@@ -80,7 +76,7 @@ public class UserJdbcDAOIntegrationTest {
         // when
         userDAO.delete(id);
         // then
-        assertNull(userDAO.findById(id));
+        assertThrows(EmptyResultDataAccessException.class, () -> userDAO.findById(id));
     }
 
     // Test findUserById-------------------------------------------------
@@ -114,19 +110,15 @@ public class UserJdbcDAOIntegrationTest {
         return String.join("", Files.readAllLines(Paths.get(SQL_SCRIPT_FILE_NAME)));
     }
 
-    private void executeScript(String script) throws SQLException {
-        try (Connection conn = dataSource.getConnection()) {
-            try (Statement statement = conn.createStatement()) {
-                statement.execute(script);
-            }
-        }
+    private void executeScript(String script) {
+        jdbcTemplate.execute(script);
     }
 
-    private void createTableUsers() throws IOException, SQLException {
+    private void createTableUsers() throws IOException {
         executeScript(readScriptFile());
     }
 
-    private void clearTableUsers() throws SQLException {
+    private void clearTableUsers() {
         executeScript("TRUNCATE TABLE users;");
     }
 }
