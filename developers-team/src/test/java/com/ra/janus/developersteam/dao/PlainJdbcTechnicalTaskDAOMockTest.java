c41f911895bae292d5f6e1180147fa50babe34b8
Andrii <andrew.ryabchuk@gmail.com>
2019-06-12 22:14:52 +0300

    JAN 41 mock tests back to shitty copypaste

--- developers-team/src/test/java/com/ra/janus/developersteam/dao/PlainJdbcTechnicalTaskDAOMockTest.java
+++ developers-team/src/test/java/com/ra/janus/developersteam/dao/PlainJdbcTechnicalTaskDAOMockTest.java
@@ -1,52 +1,178 @@
 package com.ra.janus.developersteam.dao;
 
-import com.ra.janus.developersteam.entity.BaseEntity;
 import com.ra.janus.developersteam.entity.TechnicalTask;
+import org.junit.jupiter.api.Test;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.PreparedStatementSetter;
+import org.springframework.jdbc.support.KeyHolder;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-class PlainJdbcTechnicalTaskDAOMockTest extends BaseDAOMockTest {
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
-    private TechnicalTask task;
+class PlainJdbcTechnicalTaskDAOMockTest {
 
-    @Override
-    protected BaseEntity getTestEntity() {
-        if (task == null)
-            task = new TechnicalTask(testId, "title", "description");
-        return task;
-    }
-    @Override
-    protected BaseDao getDAO() {
-        BaseDao<TechnicalTask> taskDAO = new PlainJdbcTechnicalTaskDAO(mockTemplate);
-        return taskDAO;
-    }
-    @Override
-    protected String getInsertSql() {
-        return "INSERT INTO tasks (title, description) VALUES (?, ?)";
-    }
-    @Override
-    protected String getUpdateSql() {
-        return "UPDATE tasks SET title=?,description=? WHERE id=?";
-    }
-    @Override
-    protected String getSelectAllSql() {
-        return "SELECT * FROM tasks";
-    }
-    @Override
-    protected String getSelectOneSql() {
-        return "SELECT * FROM tasks WHERE id = ?";
-    }
-    @Override
-    protected String getDeleteSql() {
-        return "DELETE FROM tasks WHERE id=?";
-    }
-    @Override
+    private static final String INSERT_SQL = "INSERT INTO tasks (title, description) VALUES (?, ?)";
+    private static final String UPDATE_SQL = "UPDATE tasks SET title=?,description=? WHERE id=?";
+    private static final String SELECT_ALL_SQL = "SELECT * FROM tasks";
+    private static final String SELECT_ONE_SQL = "SELECT * FROM tasks WHERE id = ?";
+    private static final String DELETE_SQL = "DELETE FROM tasks WHERE id=?";
+    private static final long TEST_ID = 1L;
+    private static final TechnicalTask TEST_TASK = new TechnicalTask(TEST_ID, "title", "description");
+
+    private JdbcTemplate mockTemplate = mock(JdbcTemplate.class);
+    private Connection mockConnection = mock(Connection.class);
+    private PreparedStatement mockPreparedStatement = mock(PreparedStatement.class);
+
+    private BaseDao<TechnicalTask> taskDAO = new PlainJdbcTechnicalTaskDAO(mockTemplate);
+
     protected Map<String, Object> getTestEntityMap() {
         Map<String, Object> testMap = new HashMap<>(1);
-        TechnicalTask task = (TechnicalTask) getTestEntity();
-        testMap.put("id", task.getId());
-        testMap.put("title", task.getTitle());
-        testMap.put("description", task.getDescription());
+        testMap.put("id", TEST_TASK.getId());
+        testMap.put("title", TEST_TASK.getTitle());
+        testMap.put("description", TEST_TASK.getDescription());
         return testMap;
+    }
+
+    @Test
+    void whenCreateTechnicalTaskShouldReturnTechnicalTask() throws Exception {
+        //given
+        when(mockConnection.prepareStatement(INSERT_SQL, Statement.RETURN_GENERATED_KEYS))
+                .thenReturn(mockPreparedStatement);
+        when(mockTemplate.update(any(PreparedStatementCreator.class), any(KeyHolder.class))).thenAnswer(
+                new Answer() {
+                    public Object answer(InvocationOnMock invocation) throws SQLException {
+                        Object[] args = invocation.getArguments();
+                        PreparedStatementCreator creator = (PreparedStatementCreator) args[0];
+                        creator.createPreparedStatement(mockConnection);
+
+                        KeyHolder holder = (KeyHolder) args[1];
+                        Map<String, Object> map = new HashMap<>(1);
+                        map.put("Something like a generated key", Long.valueOf(1L));
+                        holder.getKeyList().add(map);
+                        return 1;
+                    }
+                });
+
+        //when
+        TechnicalTask task = taskDAO.create(TEST_TASK);
+
+        //then
+        assertEquals(TEST_TASK, task);
+    }
+
+    //==============================
+
+    @Test
+    void whenReadTechnicalTaskFromDbByIdThenReturnIt() throws Exception {
+        //given
+        when(mockTemplate.queryForObject(eq(SELECT_ONE_SQL), any(BeanPropertyRowMapper.class), eq(TEST_ID)))
+                .thenReturn(TEST_TASK);
+
+        //when
+        TechnicalTask task = taskDAO.get(TEST_ID);
+
+        //then
+        assertEquals(TEST_ID, task.getId());
+    }
+
+    @Test
+    void whenReadAbsentTechnicalTaskFromDbByIdThenReturnNull() throws Exception {
+        //given
+        when(mockTemplate.queryForObject(eq(SELECT_ONE_SQL), any(BeanPropertyRowMapper.class), eq(TEST_ID)))
+                .thenThrow(new EmptyResultDataAccessException(1));
+
+        //when
+        TechnicalTask task = taskDAO.get(TEST_ID);
+
+        //then
+        assertEquals(null, task);
+    }
+
+    @Test
+    void whenReadAllTechnicalTasksFromDbThenReturnNonEmptyList() throws Exception {
+        //given
+        List<Map<String, Object>> rows = new ArrayList<>();
+        rows.add(getTestEntityMap());
+        when(mockTemplate.queryForList(SELECT_ALL_SQL)).thenReturn(rows);
+
+        //when
+        List<TechnicalTask> list = taskDAO.getAll();
+
+        //then
+        assertFalse(list.isEmpty());
+    }
+
+    @Test
+    void whenUpdateTechnicalTaskInDbThenReturnTrue() throws Exception {
+        //given
+        when(mockTemplate.update(eq(UPDATE_SQL), any(PreparedStatementSetter.class))).thenAnswer(new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                Object[] args = invocation.getArguments();
+                PreparedStatementSetter setter = (PreparedStatementSetter) args[1];
+                setter.setValues(mockPreparedStatement);
+                return 1;
+            }
+        });
+
+        //when
+        boolean updated = taskDAO.update(TEST_TASK);
+
+        //then
+        assertTrue(updated);
+    }
+
+    @Test
+    void whenUpdateTechnicalTaskInDbThenReturnFalse() throws Exception {
+        //given
+        when(mockTemplate.update(eq(UPDATE_SQL), any(PreparedStatementSetter.class))).thenReturn(0);
+
+        //when
+        boolean updated = taskDAO.update(TEST_TASK);
+
+        //then
+        assertFalse(updated);
+    }
+
+    @Test
+    void whenDeleteTechnicalTaskFromDbThenReturnTrue() throws Exception {
+        //given
+        when(mockTemplate.update(DELETE_SQL, TEST_ID)).thenReturn(1);
+
+        //when
+        boolean deleted = taskDAO.delete(TEST_ID);
+
+        //then
+        assertTrue(deleted);
+    }
+
+    @Test
+    void whenDeleteTechnicalTaskFromDbThenReturnFalse() throws Exception {
+        //given
+        when(mockTemplate.update(DELETE_SQL, TEST_ID)).thenReturn(0);
+
+        //when
+        boolean deleted = taskDAO.delete(TEST_ID);
+
+        //then
+        assertFalse(deleted);
     }
 }
