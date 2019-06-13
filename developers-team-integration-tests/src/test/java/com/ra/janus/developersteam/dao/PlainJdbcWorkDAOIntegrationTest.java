0e5a18e3817f46af7539a9d205463240adffd5f6
Zhurylo Mykhailo <dir2000@ukr.net>
2019-06-13 17:18:44 +0300

    JAN-41 remove unused imports

--- developers-team-integration-tests/src/test/java/com/ra/janus/developersteam/dao/PlainJdbcWorkDAOIntegrationTest.java
+++ developers-team-integration-tests/src/test/java/com/ra/janus/developersteam/dao/PlainJdbcWorkDAOIntegrationTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.math.BigDecimal;
