889a79d4b496fc07ba5fbb1247b79ad9be661e33
Oleg Kosenkov <o.kosenkov357@gmail.com>
2019-06-15 00:48:10 +0300

    JAN-51 DataSourceConfig bean reading properties file created.

--- train-tickets/src/main/java/com/ra/course/janus/traintickets/MainSpringConfig.java
+++ train-tickets/src/main/java/com/ra/course/janus/traintickets/MainSpringConfig.java
@@ -1,20 +1,10 @@
 package com.ra.course.janus.traintickets;
 
-import com.ra.course.janus.traintickets.configuration.DataSourceFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.JdbcTemplate;
-
-import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan("com.ra.course.janus.traintickets")
 public class MainSpringConfig {
-
-    @Bean
-    public DataSource dataSource() {
-        return DataSourceFactory.DATA_SOURCE.getInstance();
-    }
 
 }
---
+++ train-tickets/src/main/java/com/ra/course/janus/traintickets/configuration/DataSourceConfig.java
@@ -0,0 +1,29 @@
+package com.ra.course.janus.traintickets.configuration;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+
+import javax.sql.DataSource;
+
+@Configuration
+@PropertySource("classpath:datasource.properties")
+public class DataSourceConfig {
+
+    @Autowired
+    private Environment environment;
+
+    @Bean
+    public DataSource getDataSource() {
+        HikariDataSource ds = new HikariDataSource();
+
+        ds.setJdbcUrl(environment.getProperty("db.url"));
+        ds.setUsername(environment.getProperty("db.user"));
+        ds.setPassword(environment.getProperty("db.password"));
+
+        return ds;
+    }
+}
