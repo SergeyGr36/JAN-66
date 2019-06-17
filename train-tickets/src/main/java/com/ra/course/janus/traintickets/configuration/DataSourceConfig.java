fcee6f020342305d96ed160ed8b10f3ae1cd7aa3
Oleg Kosenkov <o.kosenkov357@gmail.com>
2019-06-18 00:00:22 +0300

    JAN-50 DatabasePopulator added to DataSource bean.

--- train-tickets/src/main/java/com/ra/course/janus/traintickets/configuration/DataSourceConfig.java
+++ train-tickets/src/main/java/com/ra/course/janus/traintickets/configuration/DataSourceConfig.java
@@ -6,6 +6,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.DatabasePopulator;
+import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 
 import javax.sql.DataSource;
 
@@ -14,16 +18,23 @@ import javax.sql.DataSource;
 public class DataSourceConfig {
 
     @Autowired
-    private Environment environment;
+    private Environment env;
 
     @Bean
     public DataSource getDataSource() {
         HikariDataSource ds = new HikariDataSource();
 
-        ds.setJdbcUrl(environment.getProperty("db.url"));
-        ds.setUsername(environment.getProperty("db.user"));
-        ds.setPassword(environment.getProperty("db.password"));
+        ds.setJdbcUrl(env.getProperty("db.url"));
+        ds.setUsername(env.getProperty("db.user"));
+        ds.setPassword(env.getProperty("db.password"));
 
+        DatabasePopulatorUtils.execute(createDatabasePopulator(), ds);
         return ds;
+    }
+
+    private DatabasePopulator createDatabasePopulator() {
+        final ResourceDatabasePopulator databasePopulator = new ResourceDatabasePopulator();
+        databasePopulator.addScript(new ClassPathResource("create_schema.sql"));
+        return databasePopulator;
     }
 }
